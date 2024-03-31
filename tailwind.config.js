d866712b6652ad5616611429fb1199996a635476
Afiq Hamzah <afiq.hamzah@pkb.net.my>
2024-03-31 10:53:57 +0800

    Fix cannot use tailwind css classes directly

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ export default {
         "./app/Livewire/**/**/*.php",
 
         // Add mary
-        "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
+        "./vendor/robsontenorio/mary/src/View/Components/**/*.php",
+
+        // Add laravel pagination
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
     ],
     theme: {
         extend: {},
@@ -21,11 +24,5 @@ export default {
     daisyui: {
         themes: ["light", "dark", "corporate", "emerald"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
         darkTheme: "corporate", // name of one of the included themes for dark mode
-        base: true, // applies background color and foreground color for root element by default
-        styled: true, // include daisyUI colors and design decisions for all components
-        utils: true, // adds responsive and modifier utility classes
-        prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-        logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-        themeRoot: ":root", // The element that receives theme color CSS variables
     },
 }
