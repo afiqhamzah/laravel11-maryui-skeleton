f2b782e81dbbdfb5e9bbc95f97aa5847df2ede1e
Afiq Hamzah <afiq.hamzah@pkb.net.my>
2024-03-31 13:32:43 +0800

    Added sample login page and register page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,21 @@
 <?php
 
 use Livewire\Volt\Volt;
+use Illuminate\Support\Facades\Route;
+
+// Users will be redirected to this route if not logged in
+Volt::route('/login', 'login')->name('login');
+
+// Define the logout
+Route::get('/logout', function () {
+    auth()->logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+
+    return redirect('/');
+});
+
+Volt::route('/register', 'register');
 
 Volt::route('/', 'users.index');
 
