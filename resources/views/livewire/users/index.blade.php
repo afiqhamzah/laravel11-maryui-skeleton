bf114ef96b6a737b9968dbf265eb2a926c952d16
Afiq Hamzah <afiq.hamzah@pkb.net.my>
2024-03-31 11:15:01 +0800

    Update user index page to use dynamic data from db and add enhancements like filters, pagination, and column sorting

--- resources/views/livewire/users/index.blade.php
+++ resources/views/livewire/users/index.blade.php
@@ -1,30 +1,43 @@
 <?php
 
 use App\Models\User;
+use App\Models\Country;
 use Illuminate\Support\Collection;
 use Livewire\Volt\Component;
 use Mary\Traits\Toast;
+use Illuminate\Database\Eloquent\Builder;
+use Livewire\WithPagination;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 new class extends Component {
     use Toast;
+    use WithPagination;
 
     public string $search = '';
 
     public bool $drawer = false;
 
+    public bool $drawer_country = false;
+
     public array $sortBy = ['column' => 'name', 'direction' => 'asc'];
+
+    public int $country_id = 0;
 
     // Clear filters
     public function clear(): void
     {
         $this->reset();
+        $this->resetPage();
         $this->success('Filters cleared.', position: 'toast-bottom');
     }
 
     // Delete action
     public function delete($id): void
     {
-        $this->warning("Will delete #$id", 'It is fake.', position: 'toast-bottom');
+        $user = User::find($id);
+        $userName = $user->name;
+        $user->delete();
+        $this->warning("$userName is deleted", position: 'toast-bottom');
     }
 
     // Table headers
@@ -33,8 +46,8 @@ new class extends Component {
         return [
             ['key' => 'id', 'label' => '#', 'class' => 'w-1'],
             ['key' => 'name', 'label' => 'Name', 'class' => 'w-64'],
-            ['key' => 'age', 'label' => 'Age', 'class' => 'w-20'],
-            ['key' => 'email', 'label' => 'E-mail', 'sortable' => false],
+            ['key' => 'country_name', 'label' => 'country', 'class' => 'lg:table-cell'],
+            ['key' => 'email', 'label' => 'E-mail', 'sortable' => false]
         ];
     }
 
@@ -44,26 +57,32 @@ new class extends Component {
      * On real projects you do it with Eloquent collections.
      * Please, refer to maryUI docs to see the eloquent examples.
      */
-    public function users(): Collection
+    public function users(): LengthAwarePaginator
     {
-        return collect([
-            ['id' => 1, 'name' => 'Mary', 'email' => 'mary@mary-ui.com', 'age' => 23],
-            ['id' => 2, 'name' => 'Giovanna', 'email' => 'giovanna@mary-ui.com', 'age' => 7],
-            ['id' => 3, 'name' => 'Marina', 'email' => 'marina@mary-ui.com', 'age' => 5],
-        ])
-            ->sortBy([[...array_values($this->sortBy)]])
-            ->when($this->search, function (Collection $collection) {
-                return $collection->filter(fn(array $item) => str($item['name'])->contains($this->search, true));
-            });
+        return User::query()
+            ->withAggregate('country', 'name')
+            ->when($this->search, fn(Builder $q) => $q->where('name', 'like', "%$this->search%"))
+            ->when($this->country_id, fn(Builder $q) => $q->where('country_id', $this->country_id))
+            ->orderBy(...array_values($this->sortBy))
+            ->paginate(5);
     }
 
     public function with(): array
     {
         return [
             'users' => $this->users(),
-            'headers' => $this->headers()
+            'headers' => $this->headers(),
+            'countries' => Country::all(),
         ];
     }
+
+    public function updated($property): void
+    {
+        if (!is_array($property) && $property != '') {
+            $this->resetPage();
+        }
+    }
+
 }; ?>
 
 <div>
@@ -73,26 +92,53 @@ new class extends Component {
             <x-input placeholder="Search..." wire:model.live.debounce="search" clearable icon="o-magnifying-glass" />
         </x-slot:middle>
         <x-slot:actions>
-            <x-button label="Filters" @click="$wire.drawer = true" responsive icon="o-funnel" />
+            <x-button  @click="$wire.drawer = true" responsive icon="o-funnel">
+                Filter
+                @if($search !== '')
+                    <x-badge value="1" class="bg-orange-600 text-white" />
+                @endif
+            </x-button>
+            <x-button  @click="$wire.drawer_country = true" responsive icon="o-funnel">
+                Filter by country
+                @if($country_id !== 0)
+                    <x-badge value="1" class="bg-orange-600 text-white" />
+                @endif
+            </x-button>
         </x-slot:actions>
     </x-header>
 
     <!-- TABLE  -->
     <x-card>
-        <x-table :headers="$headers" :rows="$users" :sort-by="$sortBy">
+
+
+
+        <x-table :headers="$headers" :rows="$users" :sort-by="$sortBy" with-pagination>
             @scope('actions', $user)
-            <x-button icon="o-trash" wire:click="delete({{ $user['id'] }})" wire:confirm="Are you sure?" spinner class="btn-ghost btn-sm text-red-500" />
+                <x-button icon="o-trash" wire:click="delete({{ $user['id'] }})" wire:confirm="Are you sure?" spinner
+                    class="btn-ghost btn-sm text-red-500" />
             @endscope
         </x-table>
     </x-card>
 
     <!-- FILTER DRAWER -->
     <x-drawer wire:model="drawer" title="Filters" right separator with-close-button class="lg:w-1/3">
-        <x-input placeholder="Search..." wire:model.live.debounce="search" icon="o-magnifying-glass" @keydown.enter="$wire.drawer = false" />
+        <x-input placeholder="Search..." wire:model.live.debounce="search" icon="o-magnifying-glass"
+            @keydown.enter="$wire.drawer = false" />
 
         <x-slot:actions>
             <x-button label="Reset" icon="o-x-mark" wire:click="clear" spinner />
             <x-button label="Done" icon="o-check" class="btn-primary" @click="$wire.drawer = false" />
         </x-slot:actions>
+    </x-drawer>
+
+
+    <x-drawer wire:model="drawer_country" title="Filters by country" right separator with-close-button class="lg:w-1/3">
+            <div class="grid gap-5">
+                <x-select placeholder="Country" wire:model.live="country_id" :options="$countries" icon="o-flag" placeholder-value="0" />
+            </div>
+            <x-slot:actions>
+                <x-button label="Reset" icon="o-x-mark" wire:click="clear" spinner />
+                <x-button label="Done" icon="o-check" class="btn-primary" @click="$wire.drawer_country = false" />
+            </x-slot:actions>
     </x-drawer>
 </div>
